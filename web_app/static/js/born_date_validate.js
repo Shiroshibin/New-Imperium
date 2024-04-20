6b9e4950364c11bed7853d2bf49655feca5b4045
Danil-114195722 <ejyou.user@gmail.com>
2024-04-20 13:52:59 +0300

    Added validation on JS for born_date

--- web_app/static/js/born_date_validate.js
+++ web_app/static/js/born_date_validate.js
@@ -1,14 +1,17 @@
 document.querySelector('form').addEventListener('submit', function(event) {
     event.preventDefault();
 
-////    const re = /(([0-2]\d)|(3[01]))(0\d|1[0-2])([12]\d{3})/;
-//    const re = new RegExp("/(([0-2]\d)|(3[01]))\.((0\d)|(1[0-2]))\.([12](\d{3}))/");
-//    const born_date_input = document.getElementById('born_date');
-//
+    const re = /^(0[1-9]|[12]\d|3[01])\.(0[1-9]|1[0-2])\.([12]\d{3})$/;;
+    const born_date_input = document.getElementById('born_date');
+
 //    console.log([document.getElementById('born_date').value]);
-////    console.log([born_date_input.match(re)]);
 //    console.log([re.test(born_date_input)]);
 
-    // Отправляем форму
-    this.submit();
+    // если введённыя дата соответствует регулярке
+    if (re.test(born_date_input.value)) {
+        // Отправляем форму
+        this.submit();
+    } else {
+        born_date_input.style.border = "3px solid red"
+    }
 });
